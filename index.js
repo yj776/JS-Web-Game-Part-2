e2572296f3c1424d46e42f6eb5ed92a987df0f8a
yj776 <yj776@nyu.edu>
2022-10-08 21:26:52 -0400

    JS-Web-Game-Part-2 Main Completed

--- index.js
+++ index.js
@@ -9,11 +9,18 @@ function newImage(url, left, bottom){
 }
 
 function newItem(url, left, bottom){
-    let item = newImage(url, left, bottom)
+    let item = newImage(url, left, bottom);
+    item.addEventListener('click',function(){
+        item.remove();
+        let inventoryItem = document.createElement('img')
+        inventoryItem.src = url
+        inventory.append(inventoryItem)
+    })
 }
 
+let inventory = document.createElement('div')
 function newInventory(){
-    let inventory = document.createElement('div')
+    // let inventory = document.createElement('div')
     inventory.style.position = 'fixed'
     inventory.style.bottom = '0px';
     inventory.style.left = '0px'
@@ -27,6 +34,9 @@ function newInventory(){
     inventory.style.backgroundColor = 'brown'
     document.body.append(inventory)
 }
+
+
+
 
 newInventory()
 newImage('assets/green-character.gif', 100, 250)
